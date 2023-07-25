5566b07fd2d54a088f10573261d8b327a60004a0
Rodrigo Aristeo <cuitlagua@gmail.com>
2023-07-25 16:45:15 -0600

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # basic-node-server
 
-This repo is for running a basic express server on google cloud platform.
-
-[cloud documentation](https://cloud.google.com/community/tutorials/run-expressjs-on-google-app-engine)
--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.get('/', (req, res) => {
+  res.send('hello world - #rodrigo')
+})
 
-app.get('/', function (req, res) {
-  res.send('Hello 🌎!');
-});
-
-// Start the server
-const server = app.listen(8080, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-  
-    console.log(`App listening at http://${host}:${port}`);
-  });
+app.listen(3000, () => console.log("server init"));
